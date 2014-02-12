fa794bdeeb9a969ab3153503567aa434f4b44b4e
Matthew McCants <mattmccants@gmail.com>
2014-02-11 22:32:50 -0600

    Implemented tearDown.

--- timetracker/tests/test_actions.py
+++ timetracker/tests/test_actions.py
@@ -8,7 +8,9 @@ class TestActions(unittest.TestCase):
 		def setUp(self):
 			pass
 
+		def tearDown(self):
+			os.remove('test.db')
+
 		def test_db_creation(self):
 			tt = TimeTracker(database_path='test.db', project='test')
 			self.assertTrue(os.path.exists('test.db'))
-			os.remove('test.db')
